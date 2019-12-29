8ebfd519b55a6d19e0bd3ade65e3e9481cc8dd4d
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-12-29 16:25:46 +0100

    Make rendered graphs prettier and update graphs

--- scripts/generate_graphs.R
+++ scripts/generate_graphs.R
@@ -1,12 +1,19 @@
 library(RSQLite)
 library(ggplot2)
 library(ggthemes)
+library(grid)
 
-theme <- theme_few(base_size = 24) +
-theme(axis.title.y=element_text(vjust=0.9),
-  axis.title.x=element_text(vjust=-0.1),
-  axis.ticks.x=element_blank(),
-  text=element_text(family="serif"))
+
+theme <- theme_economist(base_size=16) +
+        theme(legend.position = "bottom", legend.direction = "horizontal",
+              legend.box = "horizontal",
+              legend.key.size = unit(1, "cm"),
+              plot.title = element_text(family="Tahoma"),
+              text = element_text(family = "Tahoma"),
+              axis.title = element_text(size = 20),
+			  axis.text = element_text(size=16),
+              legend.text = element_text(size = 16),
+              legend.title=element_text(face = "bold", size = 16))
 
 con <- dbConnect(SQLite(), "benchmarks.db");
 benchmarks <- dbGetQuery(con, "SELECT name, id FROM benchmarks");
@@ -14,12 +21,21 @@ benchmarks <- dbGetQuery(con, "SELECT name, id FROM benchmarks");
 for(idx in 1:nrow(benchmarks)) {
 	benchmark_name <- benchmarks[[1]][idx];
 	benchmark_id <- benchmarks[[2]][idx];
-	result <- dbGetQuery(con, paste0("SELECT commits.hash, date, median FROM timings JOIN commits ON commits.hash=timings.hash WHERE benchmark_id=", benchmark_id, " ORDER BY date;"))
+	result <- dbGetQuery(con, paste0("SELECT commits.hash, date, median, timings FROM timings JOIN commits ON commits.hash=timings.hash WHERE benchmark_id=", benchmark_id, " ORDER BY date;"))
+	# obtain standard deviation
+	# result['sd'] <- as.numeric(lapply(lapply(strsplit(result[[4]], ','), as.numeric), sd))
+	# convert to date
+	result <- na.omit(result)
+	if (nrow(result) == 0) {
+		next;
+	}
 	result[[2]] <- as.Date(result[[2]])
 
 	store_path <- paste0("images/graphs/", benchmark_name, ".png")
 	print(store_path)
 	png(store_path, width=1024, height=640)
-	print(qplot(x=date, y=median, data=result, na.rm=TRUE, main=paste0("Benchmark ", benchmark_name), xlab="Date", ylab="Median Time (s)") + geom_line() + ylim(0,max(result['median'])) + theme)
+	fill <- "#4271AE"
+	line <- "#1F3552"
+	print(ggplot(result, aes(x=date, y=median)) + ggtitle(paste0("Benchmark ", benchmark_name)) + xlab('Date') + scale_y_continuous('Median Time (s)', limits=c(0, max(result['median'] * 1.3))) + geom_line(aes(x=date,y=median), size=.5, colour=line) + theme + geom_ribbon(ymin=0, aes(x=date, ymax=median), fill=fill))
 	dev.off()
 };
