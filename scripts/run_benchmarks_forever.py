b068ffeb55c0a874cc8d73c15610a7ae4ff6cbf8
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-12-31 20:09:56 +0100

    Update script to run benchmarks forever

--- scripts/run_benchmarks_forever.py
+++ scripts/run_benchmarks_forever.py
@@ -8,13 +8,8 @@ if len(path) > 0:
 os.chdir("..")
 
 while True:
-    proc = subprocess.Popen(['python', 'benchmark/run_benchmarks.py'])
+    proc = subprocess.Popen(['python3', 'scripts/run_benchmarks.py'])
     proc.wait()
     if proc.returncode == 0:
-        proc = subprocess.Popen(['python', 'benchmark/benchmark_html.py'])
-        proc.wait()
-        if proc.returncode != 0:
-            print("benchmark_html failed!")
-            exit(1)
-        os.system('./transfer.sh')
+        os.system('scripts/transfer.sh')
     time.sleep(60)
