5575ab41ee99695ebfda09a501bd4c65b93edde5
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-12-31 17:48:08 +0100

    Add transfer script

---
+++ scripts/transfer.sh
@@ -0,0 +1,12 @@
+python3 scripts/generate_minidb.py
+python3 scripts/generate_descriptions.py
+R -f generate_graphs.R
+python3 scripts/benchmark_html.py
+git add benchmarks
+git add _includes
+git add _data
+git add images/graphs/
+git add minibenchmarks.db
+git commit -m "Update benchmarks"
+git push
+
