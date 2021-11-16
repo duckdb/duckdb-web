0067030490e69c043a3dd134117da5e6d990e7e2
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-11-16 18:58:28 +0100

    Clean up configuration

--- scripts/generate_config_docs.py
+++ scripts/generate_config_docs.py
@@ -11,10 +11,43 @@ if len(sys.argv) < 2:
 
 db_path = sys.argv[1]
 
-cmd = '''
+keywords = [
+	'STANDARD',
+	'DETAILED',
+	'ALL',
+	'OPTIMIZED_ONLY',
+	'PHYSICAL_ONLY',
+	'JSON',
+	'QUERY_TREE',
+	'ASC',
+	'DESC',
+	'NULLS_FIRST',
+	'NULLS_LAST',
+	'AUTOMATIC',
+	'READ_ONLY',
+	'READ_WRITE'
+]
+
+description_replacement = 'description'
+for keyword in keywords:
+	description_replacement = f"replace({description_replacement}, '{keyword}', '**{keyword}**')"
+
+cmd = f'''
 .mode markdown
-SELECT name, description, input_type, value AS default_value FROM duckdb_settings() WHERE name NOT LIKE '%debug%' AND description NOT ILIKE '%debug%';
+SELECT substr(name, 2, (LEN(name) - 2)::int) AS name, {description_replacement} description, input_type, default_value
+FROM (
+SELECT ARRAY_AGG(name)::VARCHAR AS name, description, input_type,
+	FIRST(CASE WHEN name='memory_limit' OR name='max_memory'
+	THEN '75% of RAM'
+	ELSE value END) AS default_value
+FROM duckdb_settings()
+WHERE name NOT LIKE '%debug%' AND description NOT ILIKE '%debug%'
+GROUP BY description, input_type
+) tbl
+ORDER BY 1
+;
 '''
+
 
 res = subprocess.run(db_path, input=bytearray(cmd, 'utf8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout = res.stdout.decode('utf8').strip()
@@ -41,6 +74,8 @@ text = text.split(option_split)[0]
 text += option_split + '\n\n' + 'Below is a list of all available settings.'
 
 text += '\n\n' + stdout + '\n'
+
+text = text.replace('**QUERY_TREE**_OPTIMIZER', '**QUERY_TREE_OPTIMIZER**')
 
 with open(doc_file, 'w+') as f:
 	f.write(text)
