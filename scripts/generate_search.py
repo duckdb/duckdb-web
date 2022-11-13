c942b5faa6cd5ca14ad9ce22c5b0b7a1ddabf79d
Elliana May <me@mause.me>
2022-11-13 21:43:11 +0800

    chore: add normal_whitespace function

--- scripts/generate_search.py
+++ scripts/generate_search.py
@@ -15,6 +15,11 @@ skip_types = [
 	marko.block.HTMLBlock
 ]
 
+
+def normal_whitespace(desc: str) -> str:
+	return re.sub(r'\s+', ' ', desc.strip())
+
+
 def extract_text(parse_node):
 	if not hasattr(parse_node, 'children'):
 		return ''
@@ -28,7 +33,7 @@ def extract_text(parse_node):
 	return result
 
 def sanitize_input(text):
-	return re.sub('\s+', ' ', re.sub('[^\w\s_-]', ' ', text.lower())).strip()
+	return normal_whitespace(re.sub('[^\w\s_-]', ' ', text.lower())).strip()
 
 def extract_blurb(parse_node):
 	for child in parse_node.children:
@@ -140,8 +145,8 @@ function_list = {}
 def extract_functions(text, full_path):
 	functions = re.findall(r'\n[|]([^|\n]+)[|]([^|\n]+)[|]([^|\n]+)[|]([^|\n]+)[|]', text)
 	for function in functions:
-		name = sanitize_function(re.sub('\s+', ' ', extract_markdown_text(function[0].strip()).strip()))
-		desc = name + " - " + sanitize_desc(re.sub('\s+', ' ', extract_markdown_text(function[1].strip()).strip()))
+		name = sanitize_function(normal_whitespace(extract_markdown_text(function[0].strip()).strip()))
+		desc = name + " - " + sanitize_desc(normal_whitespace(extract_markdown_text(function[1].strip()).strip()))
 		if '--' in name:
 			continue
 		if name.lower() in ('function', 'operator'):
@@ -151,11 +156,12 @@ def extract_functions(text, full_path):
 		name = re.sub('[(][^)]*[)]', '', name)
 		function_list[name] = {
 			'title': name,
-			'text': re.sub(r'\s+', ' ', desc.strip().lower()),
+			'text': normal_whitespace(desc.lower()),
 			'category': os.path.basename(full_path).replace('.md', '').title() + " Functions",
 			'url': '/' + full_path.replace('.md', ''),
 			'blurb': sanitize_blurb(desc)
 		}
+
 
 function_dir = os.path.sep.join('docs/sql/functions'.split('/'))
 files = os.listdir(function_dir)
