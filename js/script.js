e7a9b6c5d76aa3025c05319db91a5dda02998b19
Franz-Kafka <spam@xj6652.de>
2022-12-01 09:58:51 +0100

    Update script.js
    fixed scroll-to animation and hash will be added to url.

--- js/script.js
+++ js/script.js
@@ -180,21 +180,18 @@ $(document).ready(function(){
     .not('[href="#"]')
     .not('[href="#0"]')
     .click(function(event) {
-      // On-page links
       if (
         location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
         &&
         location.hostname == this.hostname
       ) {
-        // Figure out element to scroll to
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
         if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
+          // event.preventDefault();
           $('html, body').animate({
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top-75
           }, 1000 );
         }
       }
