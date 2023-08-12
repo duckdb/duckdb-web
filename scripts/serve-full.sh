5419be2e35f9d04636ae2cb2ed28a1cdc8486368
Gabor Szarnyas <szarnyasg@gmail.com>
2023-08-12 15:26:28 +0200

    Fix serve-full.sh script

--- scripts/serve-full.sh
+++ scripts/serve-full.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-bundler exec jekyll serve --incremental --livereload --config _config.yml,_config_exclude_archive.yml
+bundler exec jekyll serve --incremental --livereload
