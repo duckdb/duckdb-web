3d46ab5b365983d64180ae66ef39ec331a7977a3
Elliana May <me@mause.me>
2023-08-16 21:30:54 +0800

    Delete transfer.sh

--- scripts/transfer.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ../benchmark-results
-mkdir -p ../benchmark-results/graphs
-R -f scripts/generate_graphs.R
-python3 scripts/generate_parquet.py
-scp -r ../benchmark-results/ duckdbdemo:/local/home/da-duckdbdemo/dbroot/
