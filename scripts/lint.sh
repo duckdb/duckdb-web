7c29cc4d41ae9ed94929b72a658a6a9ba2632b1e
Elliana May <me@mause.me>
2023-08-18 13:04:42 +0800

    add --fix flag to lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,10 +2,15 @@
 
 set -xeuo pipefail
 
+fix=${1-}
 
-npx markdownlint-cli docs/ dev/ _posts/ --config .markdownlint.jsonc --ignore docs/archive
+npx markdownlint-cli docs/ dev/ _posts/ --config .markdownlint.jsonc --ignore docs/archive $fix
 
-black scripts --skip-string-normalization
+check=''
+if [ -z $fix ]; then
+    check='--check --diff'
+fi
+black scripts --skip-string-normalization $check
 
 vale sync
 vale docs/ dev/ _posts/ --glob "!docs/archive/*"
