05d341c8f0d385d9bf5e95f2a85a109c4217d3ca
Gabor Szarnyas <gabor@duckdblabs.com>
2024-01-31 21:25:47 +0100

    Use smaller data set

--- _includes/landing-page/sql/csv-loader.sql
+++ _includes/landing-page/sql/csv-loader.sql
@@ -1,3 +1,3 @@
 -- Load CSV file, auto-detecting column name and types
 CREATE TABLE services AS
-FROM 'https://blobs.duckdb.org/data/railway-2023-02.csv.gz';
+FROM 'https://blobs.duckdb.org/data/nl-railway-2023-week-20.csv.gz';
