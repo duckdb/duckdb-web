f34d6ef63e023d3f0c98ed0ff84f0f0454e82da1
Gabor Szarnyas <szarnyasg@gmail.com>
2024-02-01 22:01:17 +0100

    Landing page: Update Parquet query

--- _includes/landing-page/sql/s3-parquet.sql
+++ _includes/landing-page/sql/s3-parquet.sql
@@ -1,4 +1,6 @@
 -- Directly query Parquet file in S3
-SELECT AS station, count(*) AS num_services
-FROM 's3://duckdb-blobs/data/nl-railway/services-2023.parquet'
+SELECT station_name, count(*) AS num_services
+FROM 's3://duckdb-blobs/train_services.parquet'
+GROUP BY ALL
+ORDER BY num_services DESC
 LIMIT 10;
