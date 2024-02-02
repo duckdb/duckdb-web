53b227d1f1e78b39639206d6f6752c165ef625ee
Elliana May <me@mause.me>
2024-02-02 17:14:51 +0800

    clean up node samples

--- _includes/landing-page/nodejs/web-service-integration.js
+++ _includes/landing-page/nodejs/web-service-integration.js
@@ -1,20 +1,16 @@
 // Web Service Integration:
 // create endpoint to generate numbers
-var express = require('express');
-var duckdb = require('duckdb');
-var app = express();
-var db = new duckdb.Database(':memory:');
-app.get('/getnumbers', function (req, res) {
-    db.all('SELECT random() AS num FROM range(10)',
-    function(a, b) {
-        if (a) { throw a; }
-        res.end(JSON.stringify(b));
-    });
-})
+const express = require("express");
+const duckdb = require("duckdb");
+const app = express();
+const db = new duckdb.Database(":memory:");
+app.get("/getnumbers", (req, res) => {
+  db.all("SELECT random() AS num FROM range(10)", (a, b) => {
+    if (a) {
+      throw a;
+    }
+    res.end(JSON.stringify(b));
+  });
+});
 
-var server = app.listen(8082, function () {
-    console.log(
-        "Go to: http://localhost:8082/getnumbers",
-        host, port
-    )
-})
+app.listen(8082, () => console.log("Go to: http://localhost:8082/getnumbers"));
