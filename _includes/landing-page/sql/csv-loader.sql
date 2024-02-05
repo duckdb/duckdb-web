abef5d777b3b39d68965e4cdc9c7e6f1ddab33ad
Gabor Szarnyas <szarnyasg@gmail.com>
2024-02-05 21:33:46 +0100

    Landing page: Use .csv.zst in example instead of .csv.gz

--- _includes/landing-page/sql/csv-loader.sql
+++ _includes/landing-page/sql/csv-loader.sql
@@ -1,3 +1,3 @@
 -- Load CSV file, auto-detecting column name and types
 CREATE TABLE stations AS
-    FROM 'https://blobs.duckdb.org/stations.csv.gz';
+    FROM 'https://blobs.duckdb.org/stations.csv.zst';
