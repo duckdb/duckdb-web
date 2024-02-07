778269cb551cd521a0b87456439043101d74ff27
eitsupi <50911393+eitsupi@users.noreply.github.com>
2024-02-08 07:55:42 +0900

    use raw string for R example

--- _includes/landing-page/r/sql-query.r
+++ _includes/landing-page/r/sql-query.r
@@ -4,11 +4,13 @@ library(duckdb)
 con <- dbConnect(duckdb())
 duckdb_register(con, "iris", iris)
 
-query <- paste(
-    "SELECT count(*) AS num_observations, ",
-    "max(\"Sepal.Width\") AS max_width, ",
-    "max(\"Petal.Length\") AS max_petal_length ",
-    "FROM iris ",
-    "WHERE \"Sepal.Length\" > 5 ",
-    "GROUP BY ALL")
+query <- r"(
+SELECT count(*) AS num_observations,
+max("Sepal.Width") AS max_width,
+max("Petal.Length") AS max_petal_length
+FROM iris
+WHERE "Sepal.Length" > 5
+GROUP BY ALL
+)"
+
 dbGetQuery(con, query)
