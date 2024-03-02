ac8ce5ec5c088adb19a5fe922cec8d3e85dd0e87
Gabor Szarnyas <szarnyasg@gmail.com>
2024-03-02 15:54:47 +0100

    Use single-quotes to define string in R example #2060

--- _includes/landing-page/r/sql-query.r
+++ _includes/landing-page/r/sql-query.r
@@ -5,10 +5,10 @@ con <- dbConnect(duckdb())
 duckdb_register(con, "iris", iris)
 
 query <- paste(
-    "SELECT count(*) AS num_observations, ",
-    "max(\"Sepal.Width\") AS max_width, ",
-    "max(\"Petal.Length\") AS max_petal_length ",
-    "FROM iris ",
-    "WHERE \"Sepal.Length\" > 5 ",
-    "GROUP BY ALL")
+    'SELECT count(*) AS num_observations, ',
+    'max("Sepal.Width") AS max_width, ',
+    'max("Petal.Length") AS max_petal_length ',
+    'FROM iris ',
+    'WHERE "Sepal.Length" > 5 ',
+    'GROUP BY ALL')
 dbGetQuery(con, query)
