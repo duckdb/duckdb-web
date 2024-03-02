adeba7c3514cfcdd69187c64dfffdb95bf6e53ba
Gabor Szarnyas <szarnyasg@gmail.com>
2024-03-02 20:03:31 +0100

    ToC script: Use entry[:text] instead of entry[:header_content]
    The :header_content property was often too short, e.g., it was cut before the first colon in the header.
    The :text property does not have this problem.

--- _plugins/toc.rb
+++ _plugins/toc.rb
@@ -4,7 +4,7 @@ Jekyll::TableOfContents::Parser.class_eval do
   def inject_anchors_into_html
     @entries.each do |entry|
       # NOTE: `entry[:id]` is automatically URL encoded by Nokogiri
-      entry[:header_content].replace(
+      entry[:text].replace(
         %(<a class="anchor" style="text-decoration: none;" href="##{entry[:id]}">#{entry[:text]}</a>)
       )
     end
