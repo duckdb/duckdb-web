e887eef604f9a5981ff4521322fd101d9985718c
Gabor Szarnyas <szarnyasg@gmail.com>
2024-06-15 16:36:06 +0200

    Linter fixes

--- scripts/use-link-tags.py
+++ scripts/use-link-tags.py
@@ -7,14 +7,14 @@ dir = os.path.dirname(filename)
 
 with open(filename, "r") as f:
     s = f.read()
-    
+
     # relative paths, e.g. ../../data/parquet/overview
     for m in re.finditer(r"\]\(([.0-9a-zA-Z].*?)([#?].*?)?\)", s):
         link_path = m.group(1)
 
         if link_path.startswith("http://") or link_path.startswith("https://"):
             continue
-        
+
         resolved_path = os.path.relpath(f"{dir}/{link_path}.md")
 
         anchor_text = m.group(2) or ""
