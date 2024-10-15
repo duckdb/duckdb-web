10284e298b0d2da560b24f81a675cf59cdd6a544
Gabor Szarnyas <szarnyasg@gmail.com>
2024-10-15 11:04:45 +0200

    Rename script

--- scripts/list_community_extensions.sh
+++ scripts/list_community_extensions.sh
@@ -2,4 +2,4 @@
 
 set -xeuo pipefail
 
-duckdb -markdown -c "SELECT format('[{}]({{% link extensions/{}.md %}})', name, name) as 'Name', format('[<span class=\"github\">GitHub</span>](https://github.com/{})', repo) as 'GitHub',  #4 as 'Description' FROM 'extensions/community_extensions.csv';" > extensions/list_extensions.md
+duckdb -markdown -c "SELECT format('[{}]({{% link community_extensions/{}.md %}})', name, name) as 'Name', format('[<span class=\"github\">GitHub</span>](https://github.com/{})', repo) as 'GitHub',  #4 as 'Description' FROM 'community_extensions/community_extensions.csv';" > community_extensions/list_extensions.md
