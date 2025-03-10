e3a4f059d025eacef141f4c67b27b205c6f14f31
Gabor Szarnyas <szarnyasg@gmail.com>
2025-03-10 14:30:57 +0100

    patch

--- js/installation.js
+++ js/installation.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	let installationData = [];
 
-	$.get('/data/installation-data-1.2.yml?121', function(data) {
+	$.get('/data/installation-data-1.2.yml?121a', function(data) {
 		// console.log(data)
 		installationData = jsyaml.load(data);
 		evaluation();
