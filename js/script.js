5711306ab1adbf3318bb20e524ff562c263ff6d7
Gabor Szarnyas <szarnyasg@gmail.com>
2026-03-09 11:09:50 +0100

    Landing page update

--- js/script.js
+++ js/script.js
@@ -676,7 +676,7 @@ $('body.documentation #main_content_wrap a.externallink').each(function () {
 			const version = $installDiv.attr("data-version");
 			const $versionSpan = $(".bottombar .version");
 			if (version) {
-				$versionSpan.text("Latest release: DuckDB " + version).show();
+				$versionSpan.text("Version of the client: " + version).show();
 			} else {
 				$versionSpan.hide();
 			}
