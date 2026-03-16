2bc8b7b84e72a25eb35b5954052751baffdc042e
Franz-Kafka <spam@xj6652.de>
2026-03-16 11:45:15 +0100

    set background-color asap

--- js/determine_color_scheme.js
+++ js/determine_color_scheme.js
@@ -2,6 +2,7 @@
     function setDarkMode() {
         document.documentElement.classList.add("darkmode");
         document.documentElement.classList.add('disable-transitions');
+        document.documentElement.style.backgroundColor = '#0d0d0d';
     }
 
     try {
